43dc6697d671300774ece29bd9cf8c226f6cf1ab
rtorrescodes <rodrigo@zirian.com>
2019-05-16 05:27:41 -0500

    Manda status en V0 a status.php en reddicom.com

--- AlarmitaWifi/src/main.cpp
+++ AlarmitaWifi/src/main.cpp
@@ -6,6 +6,12 @@ char auth[] = "df52a1ea23e04d4292b3fc528a8652d6";
 char ssid[] = "DirectoMX";
 char pass[] = "qwerty1979";
 
+BLYNK_WRITE(V0)  // this function is called when a widget, like a button, is pressed
+{
+  int pinValue = param.asInt();
+  Serial.println(pinValue);
+}
+
 void setup() {
 Serial.begin(9600);
 Blynk.begin(auth, ssid, pass, IPAddress(138,128,124,188),8080);
